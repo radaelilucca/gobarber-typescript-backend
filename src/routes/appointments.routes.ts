c5c64516a2499a803e99879c09516610e79f6c51
Lucca Radaeli <radaelidev@gmail.com>
2020-07-02 19:37:14 -0300

    Start- creating Appointments Repository

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,28 +1,23 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
+import { startOfHour, parseISO } from 'date-fns';
 
-import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
 
-const appointments: Appointment[] = [];
-
-appointmentsRouter.get('/', (req, res) => res.json(appointments));
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
 
   const parsedDate = startOfHour(parseISO(date));
 
-  const findAppointmentInSameDate = appointments.find((appointment) => isEqual(parsedDate, appointment.date));
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
 
   if (findAppointmentInSameDate) {
     return res.status(400).json({ error: 'This time is not available' });
   }
-
-  const appointment = new Appointment(provider, parsedDate);
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return res.json(appointment);
 });
