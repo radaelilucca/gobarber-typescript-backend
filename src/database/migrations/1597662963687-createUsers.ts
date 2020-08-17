e9a81e19cda0d9079c394c70f01134be318e8464
Lucca Radaeli <radaelidev@gmail.com>
2020-08-17 08:26:05 -0300

    Create migration and model for appointments

--- src/database/migrations/1597662963687-createUsers.ts
+++ src/database/migrations/1597662963687-createUsers.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class createAppointments1597660514505 implements MigrationInterface {
+export default class createUsers1597662963687 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,12 +15,17 @@ export default class createAppointments1597660514505 implements MigrationInterfa
 
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -38,6 +43,6 @@ export default class createAppointments1597660514505 implements MigrationInterfa
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
