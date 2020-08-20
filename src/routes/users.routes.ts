d93349616d313ed69d49bf69e174a3e67592f65e
Lucca Radaeli <radaelidev@gmail.com>
2020-08-20 20:55:48 -0300

    feat: serve static files through /files route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,4 +1,4 @@
-import { Router, response } from 'express';
+import { Router } from 'express';
 import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
 import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
